ebaf9f2948f8ea68d50b7d32b95f32905e57543e
Bruno Carrilho <brunodostudio@gmail.com>
2025-09-13 14:18:52 +0000

    prossiga

--- src/app/servers/components/server-form.tsx
+++ src/app/servers/components/server-form.tsx
@@ -364,10 +364,17 @@ export function ServerForm({ server }: ServerFormProps) {
         </div>
 
          <Card>
-          <CardHeader>
+          <CardHeader className="flex flex-row items-center justify-between">
             <CardTitle>Servidores</CardTitle>
+            <Button
+              type="button"
+              variant="outline"
+              onClick={() => append({ name: '', type: '', screens: 0 })}
+            >
+             Adicionar Servidor
+            </Button>
           </CardHeader>
-          <CardContent className="space-y-4">
+          <CardContent className="space-y-4 pt-4">
             {fields.map((field, index) => (
               <div key={field.id} className="grid grid-cols-[1fr_1fr_1fr_auto] gap-4 items-end p-4 border rounded-lg">
                 <FormField
@@ -414,13 +421,6 @@ export function ServerForm({ server }: ServerFormProps) {
                 </Button>
               </div>
             ))}
-            <Button
-              type="button"
-              variant="outline"
-              onClick={() => append({ name: '', type: '', screens: 0 })}
-            >
-             Adicionar Servidor
-            </Button>
           </CardContent>
         </Card>
 
