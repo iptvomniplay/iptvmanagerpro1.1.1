b690b4eada65894832ad5c3152e044aa3b53e793
Bruno Carrilho <brunodostudio@gmail.com>
2025-09-14 18:11:57 +0000

    faça

--- src/app/clients/new/page.tsx
+++ src/app/clients/new/page.tsx
@@ -1,22 +1,37 @@
 'use client';
 
+import * as React from 'react';
 import { ClientForm } from '../components/client-form';
 import { useLanguage } from '@/hooks/use-language';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { PlusCircle } from 'lucide-react';
+import { cn } from '@/lib/utils';
 
 export default function NewClientPage() {
   const { t } = useLanguage();
+  const [isFormVisible, setIsFormVisible] = React.useState(false);
 
   return (
     <div className="space-y-8">
-       <Card>
-          <CardHeader>
-              <CardTitle>{t('clientRegistration')}</CardTitle>
+      <Card>
+        <CardHeader>
+          <CardTitle>{t('clientRegistration')}</CardTitle>
+        </CardHeader>
+        <CardContent className="space-y-6">
+          <Button
+            type="button"
+            onClick={() => setIsFormVisible(!isFormVisible)}
+            className="w-48"
+          >
+            <PlusCircle className="mr-2 h-5 w-5" />
+            {t('addClient')}
+          </Button>
+          <div className={cn("pt-6", isFormVisible ? 'block' : 'hidden')}>
               <CardDescription>{t('registerNewClientDescription')}</CardDescription>
-          </CardHeader>
-          <CardContent>
-             <ClientForm client={null} />
-          </CardContent>
+              <ClientForm client={null} onSubmitted={() => setIsFormVisible(false)} />
+          </div>
+        </CardContent>
       </Card>
     </div>
   );
