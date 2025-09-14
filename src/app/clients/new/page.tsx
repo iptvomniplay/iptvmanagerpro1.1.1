d2f59310b6e55e68da432a1e28c3d3187a2c6b17
Bruno Carrilho <brunodostudio@gmail.com>
2025-09-14 18:20:30 +0000

    faça

--- src/app/clients/new/page.tsx
+++ src/app/clients/new/page.tsx
@@ -5,7 +5,7 @@ import { ClientForm } from '../components/client-form';
 import { useLanguage } from '@/hooks/use-language';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
-import { PlusCircle } from 'lucide-react';
+import { PlusCircle, ChevronsDown, ChevronsUp } from 'lucide-react';
 import { cn } from '@/lib/utils';
 
 export default function NewClientPage() {
@@ -19,14 +19,36 @@ export default function NewClientPage() {
           <CardTitle>{t('clientRegistration')}</CardTitle>
         </CardHeader>
         <CardContent className="space-y-6">
-          <Button
-            type="button"
-            onClick={() => setIsFormVisible(!isFormVisible)}
-            className="w-48"
-          >
-            <PlusCircle className="mr-2 h-5 w-5" />
-            {t('addClient')}
-          </Button>
+          <div className="flex justify-between items-center">
+            <Button
+              type="button"
+              onClick={() => setIsFormVisible(true)}
+              className="w-48"
+              disabled={isFormVisible}
+            >
+              <PlusCircle className="mr-2 h-5 w-5" />
+              {t('addClient')}
+            </Button>
+
+            {isFormVisible && (
+              <Button
+                variant="ghost"
+                onClick={() => setIsFormVisible(!isFormVisible)}
+              >
+                {isFormVisible ? (
+                  <>
+                    <ChevronsUp className="mr-2 h-5 w-5" />
+                    {t('collapse')}
+                  </>
+                ) : (
+                  <>
+                    <ChevronsDown className="mr-2 h-5 w-5" />
+                    {t('expand')}
+                  </>
+                )}
+              </Button>
+            )}
+          </div>
           <div className={cn("pt-6", isFormVisible ? 'block' : 'hidden')}>
               <CardDescription>{t('registerNewClientDescription')}</CardDescription>
               <ClientForm client={null} onSubmitted={() => setIsFormVisible(false)} />
