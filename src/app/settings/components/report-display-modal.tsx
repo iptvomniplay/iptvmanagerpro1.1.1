52f72904c71a394199fc04a94f2322bb712e2fbe
Bruno Carrilho <brunodostudio@gmail.com>
2025-09-21 17:32:37 +0000

    ao clicar no botão imprimiar abre algo muito rapido e fecha nem da pra i

--- src/app/settings/components/report-display-modal.tsx
+++ src/app/settings/components/report-display-modal.tsx
@@ -73,7 +73,6 @@ export function ReportDisplayModal({ isOpen, onClose }: ReportDisplayModalProps)
         printWindow.document.close();
         printWindow.focus();
         printWindow.print();
-        printWindow.close();
       }
     }
   };
